1a1c23b4cab019c5370c499181be35b3cdcc4192
igor-cotrim <igorxuxicotrim@gmail.com>
2021-03-21 12:20:00 -0300

    made to page about

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
-import { GetServerSidePropsContext, GetStaticPropsContext } from 'next'
+// import { GetServerSidePropsContext, GetStaticPropsContext } from 'next'
 import {services} from '../data'
+import ServiceCard from '../src/components/ServiceCard'
 
-const index = (props) => {
-  console.log(services)
-
+const index = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex flex-col flex-grow px-6 pt-1">
+      <h5 className="my-3 font-medium">
+        Sou aluno do bacharelado em Sistemas de Informação 🎓 pelo Instituto Federal da Bahia - IFBA 🏛.
+        Sou um aluno apaixonado que está sempre disposto a aprender e trabalhar com tecnologias e domínios 💡.
+        Adoro explorar novas tecnologias e tirar proveito delas para resolver problemas da vida real ✨.
+        Atualmente estou trabalhando em Desenvolvimento Web 🕸️ e sempre querendo melhorar!
+      </h5>
+      <div className="flex-grow p-4 mt-5 bg-gray-400" style={{marginLeft: '-1.5rem', marginRight: '-1.5rem'}}>
+        <h6 className="my-3 text-xl font-bold tracking-wide">O que eu ofereço:</h6>
+        <div className="grid gap-6 lg:grid-cols-2">
+          {services.map(service => (
+            <div className="bg-gray-200 rounded-lg lg:col-span-1">
+              <ServiceCard service={service}/>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
